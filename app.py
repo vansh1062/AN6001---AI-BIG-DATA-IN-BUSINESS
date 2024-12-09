435e3a5b2782535c0ea752b1922ac3edea471983
Vansh Agarwal <78074384+vansh1062@users.noreply.github.com>
2024-12-09 12:47:40 +0000

    Commit 5.2

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask # type: ignore
 from flask import render_template,request
 import textblob as tb
+import google.generativeai as genAI  # type: ignore
+import os
+
+api = os.getenv("makersuite")
+genAI.configure(api_key = "api")
+model = genAI.GenerativeModel("gemini-1.5-flash-002")
 
 app = Flask("__name__")
 
@@ -22,6 +28,16 @@ def SA_result():
     q = request.form.get("q")
     r = tb.TextBlob(q).sentiment
     return(render_template("SA_result.html", r=r))
+
+@app.route("/genAI", methods=["GET", "POST"])
+def genAI():
+    return(render_template("genAI.html"))
+
+@app.route("/genAI_result", methods=["GET", "POST"])
+def genAI_result():
+    q = request.form.get("q")
+    r = model.generate_content(q)
+    return(render_template("genAI_result.html", r=r.candidate[0].content.parts[0].text))
 
 if __name__ == "__main__":
     app.run()
